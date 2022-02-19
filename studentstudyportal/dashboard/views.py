a4870b300acf8134842c9d5db1b6acab99b84f49
valera122333 <vgromoglasov@bk.ru>
2022-02-20 01:31:56 +1000

    первый коммит,создали первую страницу

--- studentstudyportal/dashboard/views.py
+++ studentstudyportal/dashboard/views.py
@@ -1,7 +1,28 @@
+from email import message
+from turtle import title
 from django.shortcuts import render
-
+# from .models import Notes
+from .forms import *
+from django.contrib import messages
 # Create your views here.
 
 
 def home(request):
     return render(request, 'dashboard/home.html')
+
+
+def notes(request):
+    if request.method == 'POST':
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(
+                user=request.user, title=request.POST['title'], description=request.POST['description'])
+            notes.save()
+        messages.success(
+            request, f'вы успешно добавили {request.user.username} новую задачу!')
+    else:
+
+        form = NotesForm()
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes': notes, 'form': form}
+    return render(request, 'dashboard/notes.html', context)
